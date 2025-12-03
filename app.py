97e30249003629e0c685b9453f913ce6966c00bf
mciorast <marian.ciorasteanu@oracle.com>
2025-12-03 22:58:37 +0200

    lastest updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello from demo-app running in container within Cloud RUN!"
+    return "<h1>Hello from demo-app running in container within Cloud RUN! <h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
